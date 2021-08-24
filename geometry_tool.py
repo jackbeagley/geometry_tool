7f512b3f6b6da2d9152fac52837de8f42533a4fe
Jack Beagley <jack.beagley@outlook.com>
2021-08-24 17:58:38 +1200

    Commented functions, made docstrings

--- geometry_tool.py
+++ geometry_tool.py
@@ -9,104 +9,216 @@
 #
 #############################################################################
 
+import enum
 import math
 import matplotlib.pyplot as plt
 import numpy as np
 import ogr, osr
 import pandas as pd
 import re
-import shapely
 
 from scipy.interpolate import splprep, splev
 
+class PicReaderState(enum.Enum):
+	"""
+	State enum to keep track of the .pic file reader
+	"""
+	not_initialised = 0
+	ensemble_information = 1
+	header = 2
+	data = 3
+	blank =  4
+
 def rotation_matrix(theta):
+	"""
+	Output a numpy array containing a rotation matrix `A`.
+
+	This matrix is to be used on coordinates, rotating them clockwise
+	about the origin, such as:
+
+	`p' = A * p`
+
+	Parameters
+	----------
+	theta: angle (in radians) by which to rotate points
+
+	Returns
+	-------
+	Numpy rotation matrix corresponding to theta
+	"""
 	theta_rad = np.radians(theta)
 	c, s = np.cos(theta_rad), np.sin(theta_rad)
+
 	return np.array(((c, s), (-s, c)))
 
 def get_hydrophone_location(df, i, channel, channel_spacing = 3.125):
+	"""
+	Retrace the first hydrophones (channel = 1) locus to determine where a
+	specified channel will be
+
+	Parameters
+	----------
+	df: Dataframe containing streamer, source and navigation locations
+
+	i: Index of `df` to consider
+
+	channel: Hydrophone channel to determine locaiton of
+
+	channel_spacing: Spacing between hydrophone channels on streamer.
+		Default for Otago University's Geometrics MicroEel is 3.125m
+
+	Returns
+	-------
+	Two element numpy array containing Northing and Easting of the hydrophone location
+	"""
+	# channel starts at 1, so the offset is calculated as below
 	streamer_offset = (channel - 1) * channel_spacing
 	
+	# Create a counter to step back into the dataframe
 	j = i
 
+	# To keep track of the streamer offset as we trace back along the streamer
 	current_streamer_offset = 0.0
-	location_found = False
 
 	while (j >= 1):
+		# Calculate the spacing between subsequent streamer locations
 		streamer_spacing = math.sqrt((df['streamer_e'].iloc[j] - df['streamer_e'].iloc[j - 1]) ** 2 + (df['streamer_n'].iloc[j] - df['streamer_n'].iloc[j - 1]) ** 2)
 		
+		# Determine if the hydrophone is within this interval or not
 		if (current_streamer_offset + streamer_spacing) > streamer_offset:
-			fractional_offset = (streamer_offset - current_streamer_offset) / streamer_spacing
+			# Calculate where in this interval (as a ratio) the hydrophone is
+			x = (streamer_offset - current_streamer_offset) / streamer_spacing
 			
-			return np.array([df['streamer_e'].iloc[j] * (1 - fractional_offset) + df['streamer_e'].iloc[j - 1] * fractional_offset, df['streamer_n'].iloc[j] * (1 - fractional_offset) + df['streamer_n'].iloc[j - 1] * fractional_offset])
+			hydrophone_location_e = df['streamer_e'].iloc[j] * (1 - x) + df['streamer_e'].iloc[j - 1] * x
+			hydrophone_location_n = df['streamer_n'].iloc[j] * (1 - x) + df['streamer_n'].iloc[j - 1] * x
+
+			# Return the location
+			return np.array([hydrophone_location_e, hydrophone_location_n])
 		
+		# Increment the current offset along the streamer locus
 		current_streamer_offset = current_streamer_offset + streamer_spacing
 		j = j - 1
 	
+	# If the end of the locus has been reached, determine how much offset is remaining
 	remaining_offset = streamer_offset - current_streamer_offset
 
-	return np.array([df['streamer_e'].iloc[0] + remaining_offset * math.sin(math.radians(df['heading'].iloc[0] - 180.0)), df['streamer_n'].iloc[0] + remaining_offset * math.cos(math.radians(df['heading'].iloc[0] - 180.0))])
+	# Use the heading at the start of the survey to project back an estimated location of the hydrophone
+	hydrophone_location_e = df['streamer_e'].iloc[0] + remaining_offset * math.sin(math.radians(df['heading'].iloc[0] - 180.0))
+	hydrophone_location_n = df['streamer_n'].iloc[0] + remaining_offset * math.cos(math.radians(df['heading'].iloc[0] - 180.0))
+
+	# Return the location
+	return np.array([hydrophone_location_e, hydrophone_location_n])
 
 def get_line_length(x, y):
+	"""
+	Get the length of a line specified by x and y coordinates by
+	adding together the distance between subsequent points
+
+	Parameters
+	----------
+	x: Array of x coordinates
+
+	y: Array of y coordinates
+
+	Returns
+	-------
+	length of line in the same units as the input coordinates
+	"""
 	n_points = len(x)
 
 	length = 0.0
 
+	# Loop through all of the coordinates, adding the distance between them
 	for i in range(n_points - 1):
 		length = length + np.sqrt((x[i+1] - x[i])**2 + (y[i+1] - y[i])**2)
 	
+	# Return the length
 	return length
 
 def read_offsets(f, first_shot, n_shots, n_channels, c = 1500.0):
+	"""
+	Read direct arricals from a .pic file (output by CLARITAS), convert
+	to offsets using a specified speed of sound and output to a
+	2D numpy array
+
+	Parameters
+	----------
+	f: File stream object for the .pic file
+
+	first_shot: Shot number for the first expected shot
+		This may not be present in the .pic if a direct arrival
+		couldn't be picked by the user
+	
+	n_shots: Number of shots in the line
+
+	n_channels: Number of seismic channels
+
+	c: Speed of sound in water
+
+	Returns
+	-------
+	2D numpy array with rows corresponding to shots and columns corresponding
+	to channels. NaN indicates that a direct arrival wasn't found
+	"""
 	f_lines = f.readlines()
 
+	# Initialise empty arrays for storing offsets, set to NaN
 	offsets_direct = np.empty((n_shots, n_channels))
 	offsets_direct[:] = np.nan
 
 	i = first_shot
 
-	# Store flag for previous line
-	# -1: not initialised
-	#  0: ensemble information
-	#  1: header (Trace Time)
-	#  2: data
-	#  3: blank line
-	previous_line = -1
+	# Remember file reader state
+	reader_state = PicReaderState.not_initialised
 
+	# Loop through all lines in the file
+	# A state machine has been implemented to read the file
 	for f_line in f_lines:
+		# Strip leading and trailing whitespace
 		f_line_stripped = f_line.strip()
 
-		if (previous_line == 1) or (previous_line == 2):
+		if (reader_state == PicReaderState.header) or (reader_state == PicReaderState.data):
+			# Use a REGEX to try extract trace number and offset from the line
 			offset_time_match = re.match("(\d+)(\s+)(\d+.\d+)", f_line_stripped)
 			
+			# If there was a match, read the information
 			if offset_time_match:
 				channel = int(offset_time_match.group(1))
 				offset_time = float(offset_time_match.group(3))
 
+				# Column index is one less than hydrophone channel
 				j = channel - 1
+				# Calculate the offset
 				offset_distance = c * offset_time / 1000.0
 
+				# Populate the offsets array
 				offsets_direct[i, j] = offset_distance
 
+		# Check if 'shot' is present in the line
 		shot_start_index = f_line_stripped.find('shot ')
 
+		# If 'shot' is present, extract the shot index from the line
 		if shot_start_index != -1:
 			shot_end_index = f_line_stripped.rfind(')')
 
+			# Get the shot index from the line and parse as an integer
 			shot = int(f_line_stripped[shot_start_index + 5:shot_end_index])
 
+			# Map this shot index to the correct row of the array to populate
 			i = shot - first_shot
 
-			previous_line = 0
+			reader_state = PicReaderState.ensemble_information
 
+		# If 'Trace' exists in the line, it is a header immediately
+		# preceding data
 		if f_line_stripped.find('Trace') != -1:
-			previous_line = 1
+			reader_state = PicReaderState.header
 		
+		# Check if the line is blank
 		if len(f_line_stripped) == 0:
-			previous_line = 3
+			reader_state = PicReaderState.blank
 
 	return offsets_direct
-
 
 # spatial reference system
 input_EPSG = 4326
