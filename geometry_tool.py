cb46bbac8ab36a89838fef948de101fcf8d4d7cc
Jack Beagley <jack.beagley@outlook.com>
2021-09-24 14:43:08 +1200

    Commenting

--- geometry_tool.py
+++ geometry_tool.py
@@ -57,7 +57,7 @@ def rotation_matrix(theta):
 
 def get_hydrophone_location(df, i, channel, channel_spacing = 3.125):
 	"""
-	Retrace the first hydrophones (channel = 1) locus to determine where a
+	Retrace the first hydrophones' (channel = 1) locus to determine where a
 	specified channel will be
 
 	Parameters
@@ -275,14 +275,18 @@ def main():
 	# CDP spacing
 	cdp_spacing = 2.0
 
+	# Read in the specified NAV file
 	line_df = pd.read_csv(args.file_in, sep='\s+', names = ['shot', 'lon_deg', 'lon_min', 'lon_ew', 'lat_deg', 'lat_min', 'lat_ns', 'time'], skiprows=1)
 
+	# Calculate the number of shots and records
 	n_shots = len(line_df.index)
 	n_records = n_shots * n_channels
 
+	# Save the first and last shot
 	first_shot = line_df['shot'].iloc[0]
 	last_shot = line_df['shot'].iloc[-1]
 	
+	# If the user has specified direct arrivals, read them in
 	if direct_enabled:
 		if verbose_enabled:
 			print('1\tReading direct arrivals...')
@@ -300,18 +304,20 @@ def main():
 		else:
 			progressbar(1, 9)
 
+	# Calculate the decimal degrees for latitude and longitude
 	line_df['lon_dd'] = line_df['lon_deg'] + line_df['lon_min'] / 60
 	line_df['lat_dd'] = -(line_df['lat_deg'] + line_df['lat_min'] / 60)
-
 
 	if verbose_enabled:
 		print('2\tReprojecting shotpoint coordinates from EPSG:%i to EPSG:%i...' % (input_EPSG, output_EPSG))
 	else:
 		progressbar(2, 9)
 
+	# Create new columns in the dataframe
 	for new_col in ['easting', 'northing', 'heading', 'boomer_e', 'boomer_n', 'streamer_e', 'streamer_n']:
 		line_df[new_col] = pd.Series(dtype=float)
 
+	# Transform the coordinates of all the points using OGR
 	for i in range(n_shots):
 		shotpoint = ogr.Geometry(ogr.wkbPoint)
 		shotpoint.AddPoint(line_df['lon_dd'].iloc[i], line_df['lat_dd'].iloc[i])
@@ -320,6 +326,7 @@ def main():
 		line_df.ix[i,'easting'] = shotpoint.GetX()
 		line_df.ix[i,'northing'] = shotpoint.GetY()
 
+	# Take a moving average of the NAV points
 	line_df['easting_filt'] = line_df['easting'].rolling(5, center=True, min_periods=1).mean()
 	line_df['northing_filt'] = line_df['northing'].rolling(5, center=True, min_periods=1).mean()
 
@@ -328,12 +335,14 @@ def main():
 	else:
 		progressbar(3, 9)
 
+	# Calculate the heading using backwards differencing
 	for i in range(1, n_shots):
 		diff_x = line_df['easting_filt'].iloc[i] - line_df['easting_filt'].iloc[i - 1]
 		diff_y = line_df['northing_filt'].iloc[i] - line_df['northing_filt'].iloc[i - 1]
 
 		line_df.ix[i, 'heading'] = (math.degrees(math.atan2(diff_x, diff_y)) + 360.0) % 360.0
 
+	# Set the first heading as the same as the second
 	line_df.ix[0, 'heading'] = line_df.ix[1, 'heading']
 
 	if verbose_enabled:
@@ -341,6 +350,8 @@ def main():
 	else:
 		progressbar(4, 9)	
 
+	# Use a rotation matrix to convert the boomer, streamer coordinates to the
+	# onto the local coordinate system
 	for i in range(n_shots):
 		boomer_offset_rotated = np.matmul(rotation_matrix(line_df['heading'].iloc[i]), boomer_offset)
 		streamer_offset_rotated = np.matmul(rotation_matrix(line_df['heading'].iloc[i]), streamer_offset)
@@ -361,6 +372,7 @@ def main():
 	else:
 		progressbar(5, 9)	
 
+	# Retrace the hydrophone channel locations for each shot
 	for i in range(n_shots):
 		shot_en = np.array([line_df.ix[i, 'boomer_e'], line_df.ix[i, 'boomer_n']])
 
@@ -385,13 +397,16 @@ def main():
 	if verbose_enabled:
 		print('\tSpline fit score is %.3f' % fp) 
 
+	# Use the spline to return points along the CDP trajectory
 	cdp_e_eval, cdp_n_eval = splev(u_eval, tck, der=0)
 
+	# Calculate the length of the CDP trajectory
 	cdp_length = get_line_length(cdp_e_eval, cdp_n_eval)
 
 	if verbose_enabled:
 		print('\tCDP spline is %.0fm long' % cdp_length)
 
+	# Determine local parameter values corresponding to the desired CDP spacing
 	u_cdp = np.arange(0.0, 1.0, cdp_spacing / cdp_length)
 	n_cdps = len(u_cdp)
 	cdps = range(1, n_cdps + 1)
@@ -403,10 +418,15 @@ def main():
 	else:
 		progressbar(7, 9)	
 
+	# Index in the CDP array for each record
 	record_cdp_indices = np.zeros_like(mps_e, dtype=int)
+	# CDP for each record
 	record_cdps = np.zeros_like(mps_e, dtype=int)
+	# Counter for traces in each CDP
 	cdp_tracecount = np.zeros_like(cdps, dtype=int)
+	# CDP trace for each record
 	record_cdptrace = np.zeros_like(mps_e, dtype=int)
+	# Processed offset (from direct arrival if available)
 	offset_final = np.zeros_like(mps_e, dtype=int)
 
 	for i in range(n_shots):
@@ -428,14 +448,18 @@ def main():
 				else:
 					offset_final[i, j] = -int(offset_estimates[i, j] * 10)
 
+			# Get the offsets in Easting and Northing
 			cdp_distance_e = cdp_e - record_en[0]
 			cdp_distance_n = cdp_n - record_en[1]
 
+			# Calculate the distances to each CDP
 			cdp_distances = np.sqrt(cdp_distance_e**2 + cdp_distance_n**2)
 
+			# Bin the trace into the closest CDP
 			record_cdp_indices[i, j] = np.argmin(cdp_distances)
 			record_cdps[i, j] = cdps[record_cdp_indices[i, j]]
 
+			# Update the CDP tracecount
 			cdp_tracecount[record_cdp_indices[i, j]] = cdp_tracecount[record_cdp_indices[i, j]] + 1
 			record_cdptrace[i, j] = cdp_tracecount[record_cdp_indices[i, j]]
 
@@ -444,6 +468,7 @@ def main():
 	else:
 		progressbar(8, 9)	
 
+	# Determine the CDP fold
 	for i in range(n_cdps):
 		cdp_fold[i] = np.sum((record_cdps == cdps[i]).flatten())
 
@@ -455,6 +480,7 @@ def main():
 
 	f_ahl = open(args.file_out, 'w')
 
+	# Write the preamble to the CLARITAS header file
 	f_ahl.write('''\
 	ADDHDR
 	Primary key : SHOTID
